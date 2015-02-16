92ec3e541deaa99465d4970b51ab51a4c72431f8
Kristofer Phleger-Schumacher <kphleger@purdue.edu>
2015-02-15 21:28:51 -0500

    New conditionals and average functions
    Non-elegant average functions to add utility have been added. Now
    below-average scores in each case are shaded darker than the rest. Also
    added a style sheet.

--- GradStudents/gradStudentsTableScript.js
+++ GradStudents/gradStudentsTableScript.js
@@ -43,15 +43,26 @@ function averageRecom(obj) {
 $(function(){
 	var JSONLink = "https://hivelab.org/static/students.json";
 	$.getJSON(JSONLink, function(data) {
-		
+		var avgGPA = averageGPA(data);
+		var avgGRE_V = averageGRE_V(data);
+		var avgGRE_Q = averageGRE_Q(data);
+		var avgEssay = averageEssay(data);
+		var avgRecom = averageRecom(data);
 		$.each(data, function(i,f) {
-			var tblRow = "<tr>" + "<td>" +f.Name + "</td>" + "<td>" + f.GPA
-			+ "</td>" + "<td>" + f.GRE_V + "</td>" + "<td> " + f.GRE_Q +
-			"</td>" + "<td>" + f.Essay + "</td>" + "<td>" + f.Recom +
+			var tblRow = "<tr>" + "<td>" +f.Name + "</td>" + "<td id='GPA" + i + "'> " + f.GPA
+			+ "</td>" + "<td id='GRE_V" + i + "'>" + f.GRE_V + "</td>" + "<td id='GRE_Q" + i + "'> " + f.GRE_Q +
+			"</td>" + "<td id='Essay" + i + "'>" + f.Essay + "</td>" + "<td id='Recom" + i + "'> " + f.Recom +
 			"</td>" + "</tr>";
 //			console.log(f.Name);
 			$(tblRow).appendTo("#entrydata tbody");
 		});
+		for (i = 0; i < data.length; i++) {
+			if (data[i].GPA <= avgGPA) document.getElementById("GPA" + i).style.backgroundColor="darkgray";
+			if (data[i].GRE_V <= avgGRE_V) document.getElementById("GRE_V" + i).style.backgroundColor="darkgray";
+			if (data[i].GRE_Q <= avgGRE_Q) document.getElementById("GRE_Q" + i).style.backgroundColor="darkgray";
+			if (data[i].Essay <= avgEssay) document.getElementById("Essay" + i).style.backgroundColor="darkgray";
+			if (data[i].Recom <= avgRecom) document.getElementById("Recom" + i).style.backgroundColor="darkgray";
+		}
 	});
 });
 
