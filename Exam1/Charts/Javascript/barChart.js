349d7c5baeca735fdb807989a68f64b052413c54
Kristofer Phleger-Schumacher <kphleger@purdue.edu>
2015-03-11 03:04:57 -0400

    better bar chart code
    bar chart is unfinished, but a better foundation exists now

--- Exam1/Charts/Javascript/barChart.js
+++ Exam1/Charts/Javascript/barChart.js
@@ -2,13 +2,20 @@
 var w = 500
 var h = 300
 var url = "https://hivelab.org/static/exam1.json";
-$.getJSON(url, function(data) {
-	console.log("it works");
+
+$.getJSON(url, function(d) {
 	var chartArea = d3.select("#barchart").append("svg")
 		.attr("width", w)
 		.attr("height", h)
-	/*var bars = d3.selectAll("rect")
-		.data(students)
+		//.style("background-color", "red");
+	var bars = chartArea.selectAll("rect")
+		.data(d)
 		.enter()
-		.append("rect")*/
+		.append("rect");
+	var rectAttributes = bars
+		.attr("x", function(d) { return (d["University B"]-60)*10; })
+		.attr("y", 0)
+		.attr("height", function(d) { return d["University A"]/2; })
+		.attr("width", 10)
+		.style("fill", "black");
 	});
