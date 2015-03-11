f55489adfea5ad588a370f2a2ffc3b8331ec7215
Kristofer Phleger-Schumacher <kphleger@purdue.edu>
2015-03-11 05:42:32 -0400

    Mostly working bar chart code
    The bar chart generating code mostly works, except for the x placement
    and width

--- Exam1/Charts/Javascript/barChart.js
+++ Exam1/Charts/Javascript/barChart.js
@@ -1,21 +1,42 @@
-//http://hivelab.org/static/exam1.json
-var w = 500
-var h = 300
+//some constants
+var w = 500;
+var h = 300;
 var url = "https://hivelab.org/static/exam1.json";
+var dispYr = 3;
 
 $.getJSON(url, function(d) {
+	//move the data to an array
+	var data = [];
+	for (i = 0; i < d.length; i++) {
+		var year = [];
+		for (var a in d[i]) {
+			if(!isNaN(d[i][a])) {
+				year.push(d[i][a]);
+			}
+		}
+		data.push(year);
+	}
+	//define the scale functions
+	var xScale = d3.scale.ordinal()
+		.domain([0, 1, 2, 3, 4])
+		.rangeBands(w, 2, 2);
+	var yScale = d3.scale.linear()
+		.domain([0, 126])
+		.range([0,h]);
+	//put in the svg element
 	var chartArea = d3.select("#barchart").append("svg")
 		.attr("width", w)
-		.attr("height", h)
-		//.style("background-color", "red");
+		.attr("height", h);
 	var bars = chartArea.selectAll("rect")
-		.data(d)
+		.data(data[dispYr])
 		.enter()
 		.append("rect");
 	var rectAttributes = bars
-		.attr("x", function(d) { return (d["University B"]-60)*10; })
-		.attr("y", 0)
-		.attr("height", function(d) { return d["University A"]/2; })
-		.attr("width", 10)
-		.style("fill", "black");
+		.attr("x", function(d) {
+			return xScale(data[dispYr].indexOf(d))*data[dispYr].indexOf(d);
+			})
+		.attr("y", function(d) { return h-yScale(d); })
+		.attr("height", function(d) { return yScale(d); })
+		.attr("width", function(d) { return xScale(0); })
+		.style("fill", "blue");
 	});
