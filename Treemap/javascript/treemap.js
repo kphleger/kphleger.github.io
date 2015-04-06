b103134655718ec7b03559b0b9e2545577e52db6
Kristofer Phleger-Schumacher <kphleger@purdue.edu>
2015-04-06 10:10:05 -0400

    coffeeTree building
    the javascript finds the regions, states, caffeinations, and types, and
    the filter function gets children for the tree.

--- Treemap/javascript/treemap.js
+++ Treemap/javascript/treemap.js
@@ -1,7 +1,32 @@
 var csvLink = "https://hivelab.org/static/coffee.csv";
 
-$.get(csvLink, function(data) {
-	var dataString = new String(data);
-	var dataArray = $.csv.toArray(dataString);
-	console.log(dataArray[0,0]);
-}, dataType='text');
+var coffeeTree = {"name": "Coffee Tree", "children": []};
+var regions = [];
+var states = [];
+var caffeinations = [];
+var types = [];
+
+
+d3.csv(csvLink, function(data) {
+	data.forEach( function(d) {
+		if( ! (regions.indexOf(d.region)+1) ) {
+			regions.push(d.region);
+		}
+		if( ! (states.indexOf(d.state)+1) ) {
+			states.push(d.state);
+		}
+		if( ! (caffeinations.indexOf(d.caffeination)+1) ) {
+			caffeinations.push(d.caffeination);
+		}
+		if( ! (types.indexOf(d.type)+1) ) {
+			types.push(d.type);
+		}
+	});
+	regions.forEach( function(d) {
+		coffeeTree.children.push({"name": d, "children": 
+			data.filter(function(obj) {
+				if( obj.region === d ) {return obj};
+			})
+		});
+	});
+});
