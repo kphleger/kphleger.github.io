fdfa701f0b3a2c62ef645be0332a0b0c3ec1346c
Kristofer Phleger-Schumacher <kphleger@purdue.edu>
2015-04-08 17:39:50 -0400

    complete tree builder
    tree is built with no errors by iterating through each json object

--- Treemap/javascript/treemap.js
+++ Treemap/javascript/treemap.js
@@ -1,38 +1,69 @@
 var csvLink = "https://hivelab.org/static/coffee.csv";
 
 var coffeeTree = {"name": "Coffee Tree", "children": []};
-var regions = [];
-var states = [];
-var caffeinations = [];
-var types = [];
+var index = [0,0,0];
 
-function children(thisParent, list, grandChildren) {
-	var newChildren = [];
-	list.forEach( function(d) {
-		newChildren.push({"name": d, "children": [grandChildren]});
-	})
-	return newChildren;
+function testFor(objList, keyValue) {
+	var bool = false;
+	if(objList === undefined) { return bool; }
+	objList.forEach( function(d) {
+		bool = (d.name === keyValue) || bool;
+	});
+	return bool;
+}
+
+function thisIndex(objList, keyValue) {
+	for(i = 0; i < objList.length; i++) {
+		if(objList[i].name === keyValue) {break;}
+	}
+	return i;
 }
 
 d3.csv(csvLink, function(data) {
 	data.forEach( function(d) {
-		if( ! (regions.indexOf(d.region)+1) ) {
-			regions.push(d.region);
-		}
-		if( ! (states.indexOf(d.state)+1) ) {
-			states.push(d.state);
-		}
-		if( ! (caffeinations.indexOf(d.caffeination)+1) ) {
-			caffeinations.push(d.caffeination);
-		}
-		if( ! (types.indexOf(d.type)+1) ) {
-			types.push(d.type);
+		if(! testFor(coffeeTree.children, d.region)) {
+			coffeeTree.children.push(
+				{"name": d.region,
+				 "children": [{"name": d.state,
+					"children": [{"name": d.caffeination,
+						"children": [{"name": d.type,
+							"children": [d]}
+						]}
+					]}
+				]}
+			);
+		} else {
+			index[0] = thisIndex(coffeeTree.children, d.region);
+			if(! testFor(coffeeTree.children[index[0]].children, d.state)) {
+				coffeeTree.children[index[0]].children.push(
+					{"name": d.state,
+					 "children": [{"name": d.caffeination,
+						"children": [{"name": d.type,
+							"children": [d]}
+						]}
+					]}
+				);
+			} else {
+				index[1] = thisIndex(coffeeTree.children[index[0]].children, d.state);
+				if(! testFor(coffeeTree.children[index[0]].children[index[1]].children, d.caffeination)) {
+					coffeeTree.children[index[0]].children[index[1]].children.push(
+						{"name": d.caffeination,
+						 "children": [{"name": d.type,
+							"children": [d]}
+						]}
+					);
+				} else {
+					index[2] = thisIndex(coffeeTree.children[index[0]].children[index[1]].children, d.caffeination);
+					if(! testFor(coffeeTree.children[index[0]].children[index[1]].children[index[2]].children, d.type)) {
+						coffeeTree.children[index[0]].children[index[1]].children[index[2]].children.push(
+							{"name": d.type,
+							 "children": [d]}
+						);
+					} else {
+						coffeeTree.children[index[0]].children[index[1]].children[index[2]].children.push(d);
+					}
+				}
+			}
 		}
 	});
-	coffeeTree.children = (
-		children(regions, children(states, children(caffeinations, children(types, ["a", "b", "c"]))))
-	);
-/*			data.filter(function(obj) {
-				if( obj.region === d ) {return obj};
-			})*/
 });
