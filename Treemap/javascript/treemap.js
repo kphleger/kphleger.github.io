bffd9a8fb15a106cc10f74aa7ff04855373f9a58
Kristofer Phleger-Schumacher <kphleger@purdue.edu>
2015-04-08 19:37:57 -0400

    Simple tree building
    Replaced large code with simple d3 code to build tree

--- Treemap/javascript/treemap.js
+++ Treemap/javascript/treemap.js
@@ -1,69 +1,48 @@
 var csvLink = "https://hivelab.org/static/coffee.csv";
 
-var coffeeTree = {"name": "Coffee Tree", "children": []};
-var index = [0,0,0];
-
-function testFor(objList, keyValue) {
-	var bool = false;
-	if(objList === undefined) { return bool; }
-	objList.forEach( function(d) {
-		bool = (d.name === keyValue) || bool;
-	});
-	return bool;
-}
-
-function thisIndex(objList, keyValue) {
-	for(i = 0; i < objList.length; i++) {
-		if(objList[i].name === keyValue) {break;}
-	}
-	return i;
-}
-
 d3.csv(csvLink, function(data) {
-	data.forEach( function(d) {
-		if(! testFor(coffeeTree.children, d.region)) {
-			coffeeTree.children.push(
-				{"name": d.region,
-				 "children": [{"name": d.state,
-					"children": [{"name": d.caffeination,
-						"children": [{"name": d.type,
-							"children": [d]}
-						]}
-					]}
-				]}
-			);
-		} else {
-			index[0] = thisIndex(coffeeTree.children, d.region);
-			if(! testFor(coffeeTree.children[index[0]].children, d.state)) {
-				coffeeTree.children[index[0]].children.push(
-					{"name": d.state,
-					 "children": [{"name": d.caffeination,
-						"children": [{"name": d.type,
-							"children": [d]}
-						]}
-					]}
-				);
-			} else {
-				index[1] = thisIndex(coffeeTree.children[index[0]].children, d.state);
-				if(! testFor(coffeeTree.children[index[0]].children[index[1]].children, d.caffeination)) {
-					coffeeTree.children[index[0]].children[index[1]].children.push(
-						{"name": d.caffeination,
-						 "children": [{"name": d.type,
-							"children": [d]}
-						]}
-					);
-				} else {
-					index[2] = thisIndex(coffeeTree.children[index[0]].children[index[1]].children, d.caffeination);
-					if(! testFor(coffeeTree.children[index[0]].children[index[1]].children[index[2]].children, d.type)) {
-						coffeeTree.children[index[0]].children[index[1]].children[index[2]].children.push(
-							{"name": d.type,
-							 "children": [d]}
-						);
-					} else {
-						coffeeTree.children[index[0]].children[index[1]].children[index[2]].children.push(d);
-					}
-				}
-			}
-		}
+	var coffeeTree = d3.nest()
+		.key(function(d) {return d.region; })
+		.key(function(d) {return d.state; })
+		.key(function(d) {return d.caffeination; })
+		.key(function(d) {return d.type; })
+		.entries(data);
+		
+//Begin Treemap generation
+/*	var margin = {top: 40, right: 10, bottom: 10, left: 10},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+	var color = d3.scale.category20c();
+
+	var treemap = d3.layout.treemap()
+		.size([width, height])
+		.sticky(true)
+		.value(function(d) { return d.size; });
+
+	var div = d3.select("body").append("div")
+		.style("position", "relative")
+		.style("width", (width + margin.left + margin.right) + "px")
+		.style("height", (height + margin.top + margin.bottom) + "px")
+		.style("left", margin.left + "px")
+		.style("top", margin.top + "px");
+
+	d3.json("flare.json", function(error, root) {
+		var node = div.datum(root).selectAll(".node")
+		.data(treemap.nodes)
+		.enter().append("div")
+		.attr("class", "node")
+		.call(position)
+		.style("background", function(d) { return d.values ? color(d.name) : null; })
+		.text(function(d) { return d.values ? null : d.; });
 	});
+});
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+      .style("top", function(d) { return d.y + "px"; })
+      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}*/
+	
 });
